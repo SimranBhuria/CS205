bbb3ea1541cded92c9377adb038180a69ee27a3b
SimranBhuria <simran.bhuria_ug18@ashoka.edu.in>
2016-09-03 22:35:51 +0530

    Delete Q2.sh

--- Homeworks/HW1/Q2.sh
+++
@@ -1,3 +0,0 @@
-#Write a script to concatenate last 5 lines of all text files present in the
-#current directory. The output file should be named as ‘allmixedLast5’
-tail -n 5 *.txt > allmixedLast5.txt
