f2c73bec53fe31810d7c3fda447c997ca27c11b7
SimranBhuria <simran.bhuria_ug18@ashoka.edu.in>
2016-09-03 22:49:00 +0530

    Update Q1.sh

--- Homeworks/HW1/Q1.sh
+++ Homeworks/HW1/Q1.sh
@@ -1,1 +1,4 @@
+#Write a script to concatenate all text files present in the current directory.
+#The output file should be named as ‘allmixed’.
+echo "Concatenating all the files in the current directory into allmixed..."
 cat *.txt > allmixed.txt
